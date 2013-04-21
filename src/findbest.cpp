de2ef348d00a6ca3d48cf133de1af8286539fb89
Karl Schmitt <krbschmitt@genome3.umd.edu>
2013-04-21 01:22:51 -0400

    Adding findbestmerge function (in findbest.cpp). Tested in test_edge.cc. Included in Makefile. test_edge.cc also includes some more modified tests

--- src/findbest.cpp
+++ src/findbest.cpp
@@ -1,6 +1,10 @@
+#ifndef _FINDBEST_H_
+#define _FINDBEST_H_
+
 #include <horient.hpp>
 #include <list>
 #include <cstdlib>
+#include <iostream>
 
 
 //It seems my indentation is off? Indicating I missed closing something somewhere??
@@ -12,54 +16,89 @@ typedef std::list<edge>::iterator edge_ptr;
 
 //We're going to be a little less efficient on this and iterate through all the edges,
 // rather thans storing a maximum link per node, and iterating through the nodes. ( O(n) vs. O(e) ) 
-edge* findbestmerge( std::list<edge>& master_edge){
+edge_ptr findbestmerge( std::list<edge>& master_edge){
 
-//Want a list of iterators, into an edge list to indicate where in master_edge list max's are
-std::list<edge_ptr> max_links;
-double avg_score, max_score;
+  //Want a list of iterators, into an edge list to indicate where in master_edge list max's are
+  std::list<edge_ptr> max_links;
+  double avg_score, max_score;
 
-//Push the first edge so we have an initial "max" 
-max_links.push_front(master_edge.cbegin());
+  //Push the first edge so we have an initial "max" 
+  max_links.push_front(master_edge.begin());
 
-//Actual max score is the edge score divided by the maximum possible links that could have been
-// averaged into it, which is the size of the two nodes multiplied
-max_score= (*master_edge.begin()).score()/(master_edge.begin()->n1->size *  master_edge.begin()->n2->size)
+  //Actual max score is the edge score divided by the maximum possible links that could have been
+  // averaged into it, which is the size of the two nodes multiplied
+  max_score= 
+    (*master_edge.begin()).score() / 
+    ( (*master_edge.begin()->n1)->size *  (*master_edge.begin()->n2)->size);
+    
+  //Iterate through the entire master edge list, storing any edges with a maximum score.
+  for(edge_ptr m_edg_it = master_edge.begin(); m_edg_it != master_edge.end(); m_edg_it++) {
 
-//Iterate through the entire master edge list, storing any edges with a maximum score.
-for(edge_ptr m_edg_it = master_edge.begin(); m_edg_it != master_edge.end(); m_edg_it++) {
+    //The averaged score on a given node is the score, divided by the maximum possible edges in it.
+    avg_score= 
+      (*m_edg_it).score() / 
+      ( (*m_edg_it->n1)->size * (*m_edg_it->n2)->size );
 
-//The averaged score on a given node is the score, divided by the maximum possible edges in it.
-avg_score= (*m_edg_it).score() / ( (*m_edg_it)->n1->size * (*m_edg_it)->n2->size );
+    //We didn't find a new maximum, loop in for.
+    if(avg_score < max_score){ continue;}
 
-//We didn't find a new maximum
-if(avg_score < max_score){ continue;}
+    //If it's equal to our max score, push it onto the list of max-edges then go to next edge
+    if(avg_score==max_score){max_links.push_front(m_edg_it); continue;}
 
-//If it's equal to our max score, push it onto the list of max-edges then go to next edge
-if(avg_score==max_score){max_links.push_front(*m_edg_it); continue;}
+    //If we didn't continue above, we found a new max!
+    //Clear contents of list, reset maxscore, and add new edge.
+    max_links.clear(); //clear contents
+    max_links.push_front(m_edg_it); //push our new first max
+    max_score=avg_score; //make avg score our new max score.
+    
+  }
 
-//If we didn't continue above, we found a new max!
-//Clear contents of list, and reset maxscore, and add new edge.
-max_links.clear(); //clear contents
-max_links.push_front(*m_edg_it); //push our new first max
-max_score=avg_score; //make avg score our new max score.
+  //We now have a list of max edges. If there is only 1, return it.
+  if(max_links.size() == 1) {return *(max_links.begin());}  //Return contents of first element in max_links
+  
+  //Otherwise we need we have more than 1 and need to decide which of these to return
+  edge_ptr tmp_return; //Iterator in edge list, so points to an edge.
+  int min_merge_loss;
+
+  //if there is more than 1 element in the list we have to pick a join element.
+
+  tmp_return= *(max_links.begin()); //set our default return as the first edge
+  if( tmp_return->merge_loss >= 0) 
+    { min_merge_loss = tmp_return->merge_loss; } //If loss set already, save it.
+  else { min_merge_loss = find_loss(tmp_return); }  // If not set, find it and save it.
+
+  for(std::list<edge_ptr>::iterator max_it=max_links.begin(); max_it != max_links.end(); max_it++) {
+
+    //Iterate through list checking if there is a minimum merge loss
+    
+    //If the merge-loss is already known for the edge, check it. 
+    if( (*max_it)->merge_loss >= 0 && (*max_it)->merge_loss < min_merge_loss ){
+      tmp_return = *max_it;
+      min_merge_loss = tmp_return->merge_loss;
+      continue;
+    }
+
+    //Otherwise we need to Calculate the merge-loss THEN check it.
+    // Note: the call to find_loss guarantees a set value in merge_loss....
+    if( min_merge_loss > find_loss(*max_it) ) {
+      tmp_return = *max_it; 
+      min_merge_loss = tmp_return->merge_loss;
+      continue; //This is sort of irrelevant but is for carefulness with later possible logic
+    }
+
+    //If they are equal... we should probably do something to decide more!
+    if( min_merge_loss == (*max_it)->merge_loss ) { /*do nothing for now*/ };
+  }
+
+  //Sanity check.
+  if(min_merge_loss < 0 ){ std::cerr<<"Something funky in find-best\n"; exit(EXIT_FAILURE);}
+
+  return tmp_return;
 
 }
+      
 
-  //We now have a list of max edges. If there is more than 1 we need to decide which of these to return
-  if(max_links.size() == 1) {return *(max_links.cbegin());}  //Return contents of first element in max_links
     
-    edge_ptr tmp_return; //Iterator in edge list, so points to an edge.
-int min_merge_loss;
-
-//if there is more than 1 element in the list we have to pick a join element.
-
-tmp_return= *(max_links.begin()); //set our default return as the first edge
-if( tmp_return->merge_loss!=NULL) { min_merge_loss = tmp_return->merge_loss; } //If loss set already, save it.
- else { min_merge_loss = findloss(*tmp_return); }  // If not set, find it and save it.
-
-for(std::list<edge_ptr>::iterator max_it=max_links.begin(); max_it != max_links.end(); max_it++) {
-
-//Iterate through list checking if there is a minimum merge loss
 
 
-
+#endif /* _FINDBEST_H_ */
--- unittests/test_edge.cc
+++ unittests/test_edge.cc
@@ -35,31 +35,126 @@ TEST(Edge, Simple) {
 
   }
 
-  // TEST(Find_loss, Simple){
+  TEST(Find_loss, Simple){
+    //In find_loss the 3 edges tested should test the conditions in
+    // line:
+    // (diff_n1_edg < 0 || diff_n2_edg < 0 ) && (diff_n1_edg > 0 || diff_n2_edg > 0 )
 
-  // const std::string content("0 1 4 0 0 0\n"
-  //                           "0 2 0 3 0 0\n"
-  //                           "1 2 5 0 0 0\n"
-  //                           "1 3 0 3 0 0\n"
-  //                           "2 3 5 0 0 0\n"
-  //                           );
+    // Edge 1 (last) should generate diff_n1_edg>0 && diff_n2_edg<0
+    // Edge 2 (2nd to last) should generate diff_n1_edg>0 && diff_n2_edg>0
+    // Edge 3 (middle) should generate two sets of :
+    //      the diff edges. 
 
-  // std::istringstream input(content);
-  // ASSERT_TRUE(input.good());
-  // std::list<edge> master_edge;
-  // node_map_type master_node;
-  // readdata(master_edge, master_node, true, input);
+  const std::string content("0 1 4 0 0 0\n"
+                            "0 2 0 3 0 0\n"
+                            "1 2 5 0 0 0\n"
+                            "1 3 0 3 0 0\n"
+                            "2 3 5 0 0 0\n"
+                            );
+
+  std::istringstream input(content);
+  ASSERT_TRUE(input.good());
+  std::list<edge> master_edge;
+  node_map_type master_node;
+  readdata(master_edge, master_node, true, input);
   
 
-  // auto it=master_edge.begin();
+  auto it=master_edge.begin();
 
-  // //Test last, 2nd to last, and 3rd to last edge for their expected loss
-  // EXPECT_EQ(3, find_loss(it));
-  // ++it;
-  // EXPECT_EQ(0, find_loss(it) );
-  // ++it;
-  // std::cout<<"test?\n";
-  // EXPECT_EQ(6, find_loss(it) );
+  //Test last, 2nd to last, and 3rd to last edge for their expected loss
+  EXPECT_EQ(-1, it->merge_loss);
+  EXPECT_EQ(3, find_loss(it));
+  EXPECT_EQ(3, it->merge_loss);
 
-  // }
+  ++it;
+  EXPECT_EQ(-1, it->merge_loss);
+  EXPECT_EQ(0, find_loss(it) );
+  EXPECT_EQ(0, it->merge_loss);
+
+  ++it;
+  EXPECT_EQ(-1, it->merge_loss);
+  EXPECT_EQ(6, find_loss(it) );
+  EXPECT_EQ(6, it->merge_loss);
+
+  //Should have far more complex Find Loss. Above just tests
+  // the actual comparison that adds. and once.
+
+  // Second thought, it _might_ be ok... since the 3rd edge to 
+  // test the find loss on DOES make the joined nodes have 
+  // TWO neighbors. And therefore add things.
+
+  //Does not test having two neighbors and NOT adding one of them.
+  //Does not test mixed edges combinations. 
+  //Not 100% sure we've fully tested the iterating while loop,
+  //  which runs through the 2 local_list's interior to edge.
+
+  }
+
+  TEST(findbest, graph1){
+    //Tests on 4-node demonstration/example error graph 
+    // for choosing least loss.
+
+    const std::string content("0 1 4 0 0 0\n"
+			      "0 2 0 3 0 0\n"
+			      "1 2 5 0 0 0\n"
+			      "1 3 0 3 0 0\n"
+			      "2 3 5 0 0 0\n"
+			      );
+    
+    std::istringstream input(content);
+    ASSERT_TRUE(input.good());
+    std::list<edge> master_edge;
+    node_map_type master_node;
+    readdata(master_edge, master_node, true, input);
+    
+    edge_ptr best_merge;
+    
+    best_merge= findbestmerge(master_edge);
+    
+    EXPECT_TRUE(comp_edge(best_merge, master_edge.begin()) );
+  }
+
+  TEST(findbest, graph2){
+    //Test on graph used for testing all the pick_flip's.
+    // find best _should_ pick edge: 5 3 0 11 0 0
+
+    const std::string content("1 2 0 3 0 0\n"
+			      "4 2 0 6 0 0\n"
+			      "5 3 0 11 0 0\n"
+			      "3 6 2 0 0 0\n"
+			      "3 7 2 0 0 0\n"
+			      "1 4 3 0 0 0\n"
+			      "2 5 2 0 0 0\n"
+			      "2 3 2 0 0 0\n");
+    
+    std::istringstream input(content);
+    ASSERT_TRUE(input.good());
+    std::list<edge> master_edge;
+    node_map_type master_node;
+    readdata(master_edge, master_node, true, input);
+    
+    edge_ptr best_merge;
+
+    best_merge= findbestmerge(master_edge);
+
+    //Iterate to correct edge.
+
+    // pointer past last in list (top) edge, 1 2 0 3 0 0
+    auto edge_it=master_edge.end(); 
+
+    --edge_it; //points to edge 1 2 0 3 0 0
+    --edge_it; //points to edge 4 2 0 6 0 0
+    --edge_it; //points to edge 5 3 0 11 0 0
+
+    //Check that it points to right edge.
+    auto node_it=master_node.find("5");
+    EXPECT_EQ( (*node_it->second).id, (*edge_it->n1)->id );
+
+    //Then best_merge should _also_ point to the same edge
+    // if everything is working right.
+    EXPECT_EQ( (*node_it->second).id, (*best_merge->n1)->id );
+
+  }
+
+
 }
